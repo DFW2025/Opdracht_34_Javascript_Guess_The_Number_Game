86c8ceea02600c43e1dd454c8a1ff579d6917405
DFW2025 <81567386+DFW2025@users.noreply.github.com>
2021-06-24 16:59:21 +0200

    Add files via upload

---
+++ Opdracht_34_Javascript_Guess_The_Number_Game/JS/guessthenumber.js
@@ -0,0 +1,63 @@
+// Javascript
+
+
+
+var calculatenumber = Math.floor(Math.random() * 2); { };
+
+function welkom() {
+                    var person = prompt("Welkom! Wat is je naam?", "Mr. Pinky Pants");
+                                 alert ( "Hey " + person + "!    ...Hoe gaat het met jou vandaag?");
+                                 return ( number());
+                   };
+
+function totziens() {
+                     alert("Dag " + person + "!    Tot de volgende keer...");
+
+                     };
+
+
+function number()  { var inputnumber = prompt("Voer een nummer in van 0 tot 25 om te beginnen met raden...", "12");
+
+
+                    if (inputnumber != calculatenumber) {
+                                                          alert(" Uw gekozen getal " + inputnumber + " is niet correct, de computer heeft het getal " + calculatenumber + " gekozen..");
+                                                          alert ("Wilt U opnieuw het getal raden?"); 
+
+                                                          var keuze = prompt ( "J/N" , "J" );
+
+                                                          if (keuze = "J") { return (number()) }
+                                                          else { alert ("Het spel is afgelopen...")};
+                                                          
+
+                    }
+
+                   else                                  {
+                                                           alert(" Uw gekozen getal " + inputnumber + " is correct, U heeft gewonnen...");
+                                                           
+                                                           
+                    }
+
+                                                           
+                 };
+
+
+
+                
+                        
+                                       
+                                          
+                   
+
+
+
+
+
+
+
+
+
+                  
+
+
+
+
